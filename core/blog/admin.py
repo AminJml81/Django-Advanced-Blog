1eaa0ef96060a4495d13255d5faacb628bcd54d6
Amin <Aminbest1381@gmail.com>
2024-08-12 17:17:39 +0330

    added blog admin

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "author",
+        "title",
+        "status",
+        "category",
+        "created_date",
+        "published_date",
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
