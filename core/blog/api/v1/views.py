e186ee2c9e534e0589f3df5b00384390f0faec3a
Amin <Aminbest1381@gmail.com>
2024-08-27 11:35:52 +0330

    use GenericAPIView for views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView)
+from rest_framework import generics, mixins
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
@@ -46,50 +48,65 @@ from blog.models import Post
 
 
 
-class PostListCreate(APIView):
+# class PostListCreate(APIView):
+#     """getting a list of posts and creating new posts"""
+
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     serializer_class = PostSerializer
+
+#     def get(self, request):
+#         """getting list of all posts"""
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+
+
+#     def post(self, request):
+#         """creating a post with provided data"""
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+
+# class PostDetail(APIView):
+#     """Getting Detail of post, edit and delete"""
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     serializer_class = PostSerializer
+
+#     def get(self, request, id):
+#         """getting post"""
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         post = self.serializer_class(post)
+#         return Response(post.data)
+    
+
+#     def put(self, request, id):
+#         """updating post via put method"""
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         serializer = PostSerializer(post, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+
+#     def delete(self, request, id):
+#         """deleting post"""
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         post.delete()
+#         return Response({'detail':'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+class PostListCreate(ListCreateAPIView):
     """getting a list of posts and creating new posts"""
 
     permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-
-    def get(self, request):
-        """getting list of all posts"""
-        posts = Post.objects.all(status=True)
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+    #serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
 
 
-    def post(self, request):
-        """creating a post with provided data"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-
-class PostDetail(APIView):
-    """Getting Detail of post, edit and delete"""
+class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
-
-    def get(self, request, id):
-        """getting post"""
-        post = get_object_or_404(Post, pk=id, status=True)
-        post = self.serializer_class(post)
-        return Response(post.data)
-    
-
-    def put(self, request, id):
-        """updating post via put method"""
-        post = get_object_or_404(Post, pk=id, status=True)
-        serializer = PostSerializer(post, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-
-    def delete(self, request, id):
-        """deleting post"""
-        post = get_object_or_404(Post, pk=id, status=True)
-        post.delete()
-        return Response({'detail':'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+    queryset = Post.objects.filter(status=True)
+    lookup_field = 'id'
