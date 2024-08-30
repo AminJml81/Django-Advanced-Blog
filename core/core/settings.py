a0cf1aa322b510b93f9b16ddb53f1428f6c50792
Amin <Aminbest1381@gmail.com>
2024-08-30 23:10:15 +0330

    added api-docs

--- core/core/settings.py
+++ core/core/settings.py
@@ -151,3 +151,5 @@ AUTH_USER_MODEL = "accounts.User"
 #         'rest_framework.permissions.IsAuthenticated',
 #     ]
 # }
+
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
@@ -23,4 +24,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-docs/', include_docs_urls(title='api sample'))
 ]
