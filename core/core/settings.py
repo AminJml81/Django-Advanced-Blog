03344fbb243559704e0f69f84ad234a4b354d1d6
Amin <Aminbest1381@gmail.com>
2024-09-04 23:14:56 +0330

    added authentication

--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # 3rd party
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 
     #local
     'accounts.apps.AccountsConfig',
--- core/core/urls.py
+++ core/core/urls.py
@@ -17,6 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="V1 API for posts and categories",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="jamali.amin@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -24,5 +42,9 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-docs/', include_docs_urls(title='api sample'))
+    path('api-docs/', include_docs_urls(title='Course API DOC')),
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
